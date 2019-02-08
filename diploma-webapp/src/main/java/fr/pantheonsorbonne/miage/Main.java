cc2b6f6f524bd05837ab4d0c7bf5428a3be428b1
mikamz <47382633+mikamz@users.noreply.github.com>
2019-02-08 12:54:17 +0100

    update sonar test correction

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -1,35 +1,23 @@
 package fr.pantheonsorbonne.miage;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.logging.Level;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
 import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.logging.Logger;
-
 import javax.servlet.Servlet;
 import javax.servlet.ServletRegistration;
-
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
 import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
-
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
+import com.itextpdf.text.log.Logger;
 import com.google.common.io.ByteStreams;
 
 /**
@@ -52,35 +40,27 @@ public class Main {
 
 		{
 			server.start();
-			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
+			String url = "http://localhost:8080/home";
+			java.awt.Desktop.getDesktop().browse(new URI(url));
+			logger.log(level.ALL,"Press any key to stop the server...");
 			System.out.println("Press any key to stop the server...");
 			System.in.read();
 		} catch (Exception e) {
 			System.err.println(e);
 		}
 	}
-
-	protected static Student getStudentData(int studentId, StudentRepository repo) {
-		// create an arrayList of the students, because iterables are too hard
-		ArrayList<Student> students = new ArrayList<>();
-		Iterables.addAll(students, repo);
+protected static Student getStudentData(StudentRepository repo) {
 
 		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
+			if (i == studentId -1) {
 				return students.get(i);
 			}
 		}
-
 		throw new NoSuchElementException();
-
 	}
-
 	protected static void handleResponse(Response response, int studentId) throws IOException {
-
 		response.setContentType("application/pdf");
-
 		Student student = getStudentData(studentId, studentRepo);
-
 		DiplomaGenerator generator = new MiageDiplomaGenerator(student);
 		try (InputStream is = generator.getContent()) {
 			try (NIOOutputStream os = response.createOutputStream()) {
@@ -88,9 +68,7 @@ public class Main {
 			}
 
 		}
-		return;
 	}
-
 	protected static void addDiplomaPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
@@ -101,11 +79,9 @@ public class Main {
 				response.setContentType("text/html; charset=utf-8");
 				response.setStatus(404);
 				response.getWriter().write("Erreur 404 : Le diplome n'existe pas pour cet utilisateur");
-
 			}
 		}, path);
 	}
-
 	protected static void addRootPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
